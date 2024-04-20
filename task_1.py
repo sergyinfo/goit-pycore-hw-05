4b2080e6af5f8a32ba4062ffeae70b82cb676a88
Sergey Porotikov <sporotikov@cordial.com>
2024-04-20 16:10:56 +0300

    task 1 done

---
+++ task_1.py
@@ -0,0 +1,35 @@
+"""
+Task 1: Fibonacci numbers
+"""
+def caching_fibonacci():
+    """
+    Return function that calculates the n-th Fibonacci number 
+    using a cache to store intermediate results.
+    """
+
+    cache = {}
+
+    def fibonacci(n: int) -> int:
+        """
+        Return the n-th Fibonacci number using a cache to store intermediate results.
+
+        :param n: int
+        :return: int
+        """
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+    return fibonacci
+
+if __name__ == "__main__":
+    fib = caching_fibonacci()
+    print(fib(10)) # 55
+    print(fib(15)) # 610
+    print(fib(50)) # 12586269025
+    print(fib(100)) # 354224848179261915075
